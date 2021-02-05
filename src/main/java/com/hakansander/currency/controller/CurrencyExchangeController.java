369c1522c3c8df991be25da4a50b280d13ce2bb9
Hakan Sander <hakansander@gmail.com>
2021-02-05 23:18:09 +0300

    Add CORS access

--- src/main/java/com/hakansander/currency/controller/CurrencyExchangeController.java
+++ src/main/java/com/hakansander/currency/controller/CurrencyExchangeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/currency")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CurrencyExchangeController {
     @Autowired
     CurrencyExchangeService currencyExchangeService;
